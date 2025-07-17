41dad280b9ba0f09a0812063a730c66c3e2203e7
toto-toto-2 <totocuttoto@gmail.com>
2025-07-17 11:31:46 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
 import random
-
-TOKEN = 'MTM5NTMyMTM3OTk3NjUxNTc0NQ.GzDdSx.mxPqSswErQBNVnx8c0oXZfOtZI039LtE_dtZwE'
+import os
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
